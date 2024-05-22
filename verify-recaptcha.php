a37d35c1d8d81509e5374a64382950cddda055fd
Enrique Ojeda Bejarano <95145099+AEnriqueOjeda@users.noreply.github.com>
2024-05-22 13:22:31 -0600

    rC1

--- verify-recaptcha.php
+++ verify-recaptcha.php
@@ -1,20 +1,15 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $recaptchaResponse = $_POST['recaptchaResponse'];
+    $secret = '6LdaIuUpAAAAAOMNA33djdq5WFktrmJ8MbbvTChV'; // Clave secreta del reCAPTCHA
 
-// Get the reCAPTCHA response
-$recaptchaResponse = $_POST['recaptchaResponse'];
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$recaptchaResponse");
+    $responseKeys = json_decode($response, true);
 
-// Verify the reCAPTCHA response
-$secretKey = '6LdaIuUpAAAAAOMNA33djdq5WFktrmJ8MbbvTChV';
-$url = 'https://www.google.com/recaptcha/api/siteverify';
-$response = file_get_contents("{$url}?secret={$secretKey}&response={$recaptchaResponse}");
-$data = json_decode($response);
-
-if ($data->success) {
-  // The reCAPTCHA was verified successfully
-  echo json_encode(['success' => true]);
-} else {
-  // The reCAPTCHA was not verified
-  echo json_encode(['success' 'success' => false]);
+    if ($responseKeys['success']) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'error-codes' => $responseKeys['error-codes']]);
+    }
 }
-
 ?>
